3a9e646adf19b547bba1520aeadc55eaeb1c7514
Richard-Adewoye <richardadewoye031@gmail.com>
2025-05-29 10:40:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title('Electricity Demand Forecasting')
 
 st.info('This is a Machine Learning app that predicts based on Multiple Energy-related factors!')
 
-df = pd.read_csv('')
+df = pd.read_csv('https://raw.githubusercontent.com/Richard-Adewoye/electricity-demand-forecacsting/refs/heads/master/df_cleaned.csv')
