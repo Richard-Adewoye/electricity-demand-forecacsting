832d3bbc3a22a94ee65ad249e8e41a8ad854b150
Richard-Adewoye <richardadewoye031@gmail.com>
2025-05-29 10:37:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title('Electricity Demand Forecasting')
 
 st.info('This is a Machine Learning app that predicts based on Multiple Energy-related factors!')
+
+df = pd.read_csv('')
